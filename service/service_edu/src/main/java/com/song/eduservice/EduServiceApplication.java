029974bea5ee48b239ad273edd0688eedc5bf8b4
explore11 <prefect_start@163.com>
2020-08-28 22:41:52 +0800

    整合swagger

--- service/service_edu/src/main/java/com/song/eduservice/EduServiceApplication.java
+++ service/service_edu/src/main/java/com/song/eduservice/EduServiceApplication.java
@@ -1,7 +1,11 @@
 package com.song.eduservice;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @program: guli_parent
@@ -10,6 +14,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @create: 2020-08-27 22:42
  **/
 @SpringBootApplication
+@ComponentScan("com.song")
 public class EduServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(EduServiceApplication.class, args);
